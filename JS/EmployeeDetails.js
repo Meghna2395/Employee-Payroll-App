819a28b5ad915430e0cd572ba9d290a8740d00e6
Meghna2395 <meghnaborkar23@gmail.com>
2021-09-02 00:56:49 +0530

    UC2

--- JS/EmployeeDetails.js
+++ JS/EmployeeDetails.js
@@ -64,3 +64,27 @@ class EmployeeDetails {
     }
 }
 
+window.addEventListener('DOMContentLoader', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (error) {
+            textError.textContent = error;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
+    });
+});
+
