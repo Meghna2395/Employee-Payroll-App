0ec8b3d581c667390af4940706c9e0a7f59feee2
Meghna2395 <meghnaborkar23@gmail.com>
2021-09-02 01:02:51 +0530

    Local Storage

--- JS/EmployeeDetails.js
+++ JS/EmployeeDetails.js
@@ -88,3 +88,69 @@ window.addEventListener('DOMContentLoader', (event) => {
     });
 });
 
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+    } catch (e) {
+        return;
+    }
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems = [];
+    allItems.forEach(item => {
+        if (item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if (employeePayrollList != undefined) {
+        employeePayrollList.push(employeePayrollData);
+    } else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+}
+
+const resetForm = () =>{
+    getSelectedValues('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name = department]');
+    setValues('#salary','');
+    setValue('#notes','');
+    setValue('#startDate','1-January-2020');
+}
+
+const unsetSelectedValues = (propertyValue)=>{
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item=>{
+        item.checked=false;
+    });
+}
+
+const setTextValue = (id, value)=>{
+    const element = document.querySelector(id);
+    element.textContent=value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value=value;
+}
+
